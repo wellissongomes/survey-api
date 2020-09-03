aa6b0cccf5e024cdeea33afe9cd5444db7077e3a
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-09-03 18:50:53 -0300

    refactor: clear database bewtween tests

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -11,6 +11,11 @@ describe('Account mongo repository', () => {
   afterAll(async () => {
     await MongoHelper.disconnect();
   });
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts');
+    await accountCollection.deleteMany({});
+  });
   test('Should return an account on sucess', async () => {
     const sut = makeSut();
     const account = await sut.add({
