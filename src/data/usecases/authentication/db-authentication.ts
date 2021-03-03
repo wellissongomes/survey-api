7e000f4f3f5aa197db4caa2bc04d095975b4aad1
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2021-03-03 17:20:31 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,12 @@
+import { Authentication, AuthenticationModel } from '../../../domain/usecases/authentication';
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+
+export class DbAuthentication implements Authentication {
+  constructor(private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+
+  async auth(authenticationModel: AuthenticationModel): Promise<string> {
+    const { email } = authenticationModel;
+    await this.loadAccountByEmailRepository.load(email);
+    return null;
+  }
+}
